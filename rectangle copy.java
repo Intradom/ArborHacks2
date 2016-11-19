917ab667cf672409f6f191fc30f213e6776d8a30
Ronak Shetty <Ronak@Ronaks-MacBook-Air.local>
2016-11-19 15:24:38 -0500

    Added function for rectangle-point collision

---
+++ rectangle copy.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+/**
+ * Write a description of class rectangle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class rectangle
+{
+    public bool rectTouch(double x, double y, double ulx, double uly, double lrx, double lry)
+    {
+        boolean in;
+        if( px <= lrx && px >= ulx && py >= uly && py <= lry)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
+   
+
+
+
+
+     
+
+     
