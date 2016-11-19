83649830cbda76f94da1420c873b6d8ae4de8675
MichaelaLH <michaelahennebury@gmail.com>
2016-11-19 17:50:51 -0500

    update BlueJ branch

--- SoundWavesBlueJ/Main.java
+++ SoundWavesBlueJ/Main.java
@@ -1,33 +1,80 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.*;
 
-/**
- * Write a description of class Main here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Main
-{
-    // instance variables - replace the example below with your own
-    private int x;
+import javax.sound.sampled.*;
+import javax.sound.sampled.LineEvent.Type;
+import javax.swing.JFrame;
 
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+public class Main {
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+	final static File drumNoise = new File("../SoundFiles/Drum/Snare_Drum.wav");
+	final static File golfNoise = new File("../SoundFiles/golf_swing.wav"); 
+	
+	public static void playSound(File f)
+	{	
+		try {
+		    AudioInputStream stream;
+		    AudioFormat format;
+		    DataLine.Info info;
+		    Clip clip;
+
+		    stream = AudioSystem.getAudioInputStream(f);
+		    format = stream.getFormat();
+		    info = new DataLine.Info(Clip.class, format);
+		    clip = (Clip) AudioSystem.getLine(info);
+		    clip.open(stream);
+		    clip.start();
+		    
+		    LineListener listener = new LineListener() {
+		        public void update(LineEvent event) {
+	                if (event.getType() == Type.STOP) {
+	                    clip.close();
+	                    event.getLine().close();
+	                }
+		        }
+		    };
+		clip.addLineListener(listener );
+		}
+		catch (Exception e) {
+		  e.printStackTrace();
+		  System.exit(1);		
+        }
+	}
+	
+	public static void main(String[] args) throws Exception 
+	{
+		JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.setSize(1024, 768);
+        
+        f.addKeyListener(new KeyListener() {
+          
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch(e.getKeyChar())
+                {
+                	case 'd':
+                		playSound(drumNoise);
+                		break;
+                	case 'g':
+                		playSound(golfNoise);
+                		break;
+                }
+            }
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+	}
 }
