806136ec7416c3590cf238a258349ead48bbb291
MichaelaLH <michaelahennebury@gmail.com>
2016-11-20 15:17:13 -0500

    Updated upper left corners

--- RectangleCircleCorners.java
+++ RectangleCircleCorners.java
@@ -32,6 +32,23 @@ public class RectangleCircleCorners
         Tempo Up (576, 630)
         Tempo Down (576, 722)
         Loop (720, 639)
+        //Piano Screen Piano Key Corners
+        //white (these are for the bottom half of the key)
+        C_a (72, 380)
+        D_s (180, 380)
+        E_d (288, 380)
+        F_f (396, 380)
+        G_g (504, 380)
+        A_h (612, 380)
+        B_j (720, 380)
+        highC_k (828, 380)
+        //black
+        Db_w (126, 92)
+        Eb_e (234, 92)
+        Gb_t (414, 92)
+        Ab_y (522, 92)
+        Bb_u (630, 92)
+        
         
     }
 }
