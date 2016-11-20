533db8086bee92af0faa3e785de27e82dd5f9475
MichaelaLH <michaelahennebury@gmail.com>
2016-11-20 15:52:05 -0500

    Outlines for buttons

---
+++ Outlines.java
@@ -0,0 +1,83 @@
+
+/**
+ * Write a description of class Outlines here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Outlines
+{
+    final Image recordDrumSOutline = ImageIO.read(new File("../stop_record_replay_outline.png"));
+    final Image stopDrumSOutline = ImageIO.read(new File("../stop_record_replay_outline.png"));
+    final Image replayDrumSOutline = ImageIO.read(new File("../stop_record_replay_outline.png"));
+    final Image loopDrumSOutline = ImageIO.read(new File("../loop_button_outline.png"));
+    final Image tempoUpDrumSOutline = ImageIO.read(new File("../tempo_up-and-down_outline.png"));
+    final Image tempoDownDrumSOutline = ImageIO.read(new File("../tempo_up-and-down_outline.png"));
+    final Image switchDrumSOutline = ImageIO.read(new File("../switch_outline.png"));
+    final Image s1DrumSOutline = ImageIO.read(new File("../8soundbuttons_outline.png"));
+    final Image s2DrumSOutline = ImageIO.read(new File("../8soundbuttons_outline.png"));
+    final Image s3DrumSOutline = ImageIO.read(new File("../8soundbuttons_outline.png"));
+    final Image s4DrumSOutline = ImageIO.read(new File("../8soundbuttons_outline.png"));
+    final Image s5DrumSOutline = ImageIO.read(new File("../8soundbuttons_outline.png"));
+    final Image s6DrumSOutline = ImageIO.read(new File("../8soundbuttons_outline.png"));
+    final Image s7DrumSOutline = ImageIO.read(new File("../8soundbuttons_outline.png"));
+    final Image s8DrumSOutline = ImageIO.read(new File("../8soundbuttons_outline.png"));
+    final Image recordPianoSOutline = ImageIO.read(new File("../stop_record_replay_outline.png"));
+    final Image stopPianoSOutline = ImageIO.read(new File("../stop_record_replay_outline.png"));
+    final Image replayPianoSOutline = ImageIO.read(new File("../stop_record_replay_outline.png"));
+    final Image loopPianoSOutline = ImageIO.read(new File("../loop_button_outline.png"));
+    final Image tempoUPPianoSOutline = ImageIO.read(new File("../tempo_up-and-down_outline.png"));
+    final Image tempoDownPianoSOutline = ImageIO.read(new File("../tempo_up-and-down_outline.png"));
+    final Image switchPianoSOutline = ImageIO.read(new File("../switch_outline.png"));
+    final Image ku1PianoSOutline = ImageIO.read(new File("../black_key_outline.png"));
+    final Image ku2PianoSOutline = ImageIO.read(new File("../black_key_outline.png"));
+    final Image ku3PianoSOutline = ImageIO.read(new File("../black_key_outline.png"));
+    final Image ku4PianoSOutline = ImageIO.read(new File("../black_key_outline.png"));
+    final Image ku5PianoSOutline = ImageIO.read(new File("../black_key_outline.png"));
+    final Image kl1PianoSOutline = ImageIO.read(new File("../cut_off_white_key_outline.png"));
+    final Image kl2PianoSOutline = ImageIO.read(new File("../cut_off_white_key_outline.png"));
+    final Image kl3PianoSOutline = ImageIO.read(new File("../cut_off_white_key_outline.png"));
+    final Image kl4PianoSOutline = ImageIO.read(new File("../cut_off_white_key_outline.png"));
+    final Image kl5PianoSOutline = ImageIO.read(new File("../cut_off_white_key_outline.png"));
+    final Image kl6PianoSOutline = ImageIO.read(new File("../cut_off_white_key_outline.png"));
+    final Image kl7PianoSOutline = ImageIO.read(new File("../cut_off_white_key_outline.png"));
+    final Image kl8PianoSOutline = ImageIO.read(new File("../cut_off_white_key_outline.png"));
+    
+
+    
+     // Visual toggles
+    boolean recordDrumSOutlineOn = false;
+    boolean stopDrumSOutlineOn = false; +
+    boolean replayDrumSOutlineOn = false; +
+    boolean loopDrumSOutlineOn = false; +
+    boolean tempoUpDrumSOutlineOn = false; +
+    boolean tempoDownDrumSOutlineOn = false; +
+    boolean switchDrumSOutlineOn = false; +
+    boolean s1DrumSOutlineOn = false; +
+    boolean s2DrumSOutlineOn = false; +
+    boolean s3DrumSOutlineOn = false; +
+    boolean s4DrumSOutlineOn = false; +
+    boolean s5DrumSOutlineOn = false; +
+    boolean s6DrumSOutlineOn = false; +
+    boolean s7DrumSOutlineOn = false; +
+    boolean s8DrumSOutlineOn = false; +
+    boolean recordPianoSOutlineOn = false; +
+    boolean stopPianoSOutlineOn = false; +
+    boolean replayPianoSOutlineOn = false; +
+    boolean loopPianoSOutlineOn = false; +
+    boolean tempoUPPianoSOutlineOn = false; +
+    boolean tempoDownPianoSOutlineOn = false; +
+    boolean switchPianoSOutlineOn = false; +
+    boolean ku1PianoSOutlineOn = false;
+    boolean ku2PianoSOutlineOn = false;
+    boolean ku3PianoSOutlineOn = false;
+    boolean ku4PianoSOutlineOn = false;
+    boolean ku5PianoSOutlineOn = false;
+    boolean kl1PianoSOutlineOn = false;
+    boolean kl2PianoSOutlineOn = false;
+    boolean kl3PianoSOutlineOn = false;
+    boolean kl4PianoSOutlineOn = false;
+    boolean kl5PianoSOutlineOn = false;
+    boolean kl6PianoSOutlineOn = false;
+    boolean kl7PianoSOutlineOn = false;
+    boolean kl8PianoSOutlineOn = false;
