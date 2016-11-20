cecea749090aaa9b2f40dae554b947bea2c481b2
MichaelaLH <michaelahennebury@gmail.com>
2016-11-20 16:27:08 -0500

    Added switch case to outlines

--- Outlines.java
+++ Outlines.java
@@ -47,11 +47,11 @@ public class Outlines
     
      // Visual toggles
     boolean recordDrumSOutlineOn = false;
-    boolean stopDrumSOutlineOn = false; +
-    boolean replayDrumSOutlineOn = false; +
-    boolean loopDrumSOutlineOn = false; +
-    boolean tempoUpDrumSOutlineOn = false; +
-    boolean tempoDownDrumSOutlineOn = false; +
+    boolean stopDrumSOutlineOn = false; 
+    boolean replayDrumSOutlineOn = false; 
+    boolean loopDrumSOutlineOn = false; 
+    boolean tempoUpDrumSOutlineOn = false; 
+    boolean tempoDownDrumSOutlineOn = false; 
     boolean switchDrumSOutlineOn = false; +
     boolean s1DrumSOutlineOn = false; +
     boolean s2DrumSOutlineOn = false; +
@@ -60,14 +60,14 @@ public class Outlines
     boolean s5DrumSOutlineOn = false; +
     boolean s6DrumSOutlineOn = false; +
     boolean s7DrumSOutlineOn = false; +
-    boolean s8DrumSOutlineOn = false; +
+    boolean s8DrumSOutlineOn = false; 
     boolean recordPianoSOutlineOn = false; +
     boolean stopPianoSOutlineOn = false; +
     boolean replayPianoSOutlineOn = false; +
     boolean loopPianoSOutlineOn = false; +
     boolean tempoUPPianoSOutlineOn = false; +
-    boolean tempoDownPianoSOutlineOn = false; +
-    boolean switchPianoSOutlineOn = false; +
+    boolean tempoDownPianoSOutlineOn = false; 
+    boolean switchPianoSOutlineOn = false; ++
     boolean ku1PianoSOutlineOn = false;
     boolean ku2PianoSOutlineOn = false;
     boolean ku3PianoSOutlineOn = false;
@@ -81,3 +81,130 @@ public class Outlines
     boolean kl6PianoSOutlineOn = false;
     boolean kl7PianoSOutlineOn = false;
     boolean kl8PianoSOutlineOn = false;
+    
+    @Override
+    public void keyReleased(KeyEvent arg0)
+    {
+        switch(arg0.getKeyChar())
+        {
+            case '2':
+                recordDrumSOutlineOn = false;
+                recordPianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case '1':
+                stopDrumSOutlineOn = false;
+                stopPianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case '3':
+                replayDrumSOutlineOn = false;
+                replayPianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case '6':
+                loopDrumSOutlineOn = false;
+                loopPianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case '5':
+                tempoUpDrumSOutlineOn = false;
+                tempoUPPianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case '4':
+                tempoDownDrumSOutlineOn = false;
+                tempoDownPianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case '7':
+                switchDrumSOutlineOn = false;
+                switchPianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'z':
+                s1DrumSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'x':
+                s2DrumSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'c':
+                s3DrumSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'v':
+                s4DrumSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'b':
+                s5DrumSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'n':
+                s6DrumSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'm':
+                s7DrumSOutlineOn = false;
+                pane.repaint();
+                break;
+            case ',':
+                s8DrumSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'w':
+                ku1PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'e':
+                ku2PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 't':
+                ku3PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'y':
+                ku4PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'u':
+                ku5PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'a':
+                kl1PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 's':
+                kl2PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'd':
+                kl3PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'f':
+                kl4PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'g':
+                kl5PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'h':
+                kl6PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'j':
+                kl7PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            case 'k':
+                kl8PianoSOutlineOn = false;
+                pane.repaint();
+                break;
+            }
+        }
